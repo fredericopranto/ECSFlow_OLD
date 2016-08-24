dbcd7d8f03fb808c18517c4fe996b0265f9fafdb
fredericopranto <fredericopranto@gmail.com>
2016-08-24 17:36:36 -0300

    create test for ascgen

---
+++ Ascgen2NoTry/Ascgen2Tests/WeaverTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class WeaverTests
+{
+    Assembly assembly;
+    string newAssemblyPath;
+    string assemblyPath;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var projectPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Ascgen2\Ascgen2.csproj"));
+        assemblyPath = Path.Combine(Path.GetDirectoryName(projectPath), @"bin\Debug\Ascgen2.exe");
+#if (!DEBUG)
+        assemblyPath = assemblyPath.Replace("Debug", "Release");
+#endif
+
+        newAssemblyPath = assemblyPath.Replace(".exe", "2.exe");
+        File.Copy(assemblyPath, newAssemblyPath, true);
+
+        var moduleDefinition = ModuleDefinition.ReadModule(newAssemblyPath);
+        var weavingTask = new ModuleWeaver
+        {
+            ModuleDefinition = moduleDefinition
+        };
+
+        weavingTask.Execute();
+        moduleDefinition.Write(newAssemblyPath);
+
+        assembly = Assembly.LoadFile(newAssemblyPath);
+    }
+
+    [TestMethod]
+    public void Ascgen2ValidateHelloWorldIsInjected()
+    {
+        var type = assembly.GetType("Hello");
+        var instance = (dynamic) Activator.CreateInstance(type);
+
+        Assert.AreEqual("Hello World", instance.World());
+    }
+
+#if(DEBUG)
+    [TestMethod]
+    public void Ascgen2PeVerify()
+    {
+        Verifier.Verify(assemblyPath,newAssemblyPath);
+    }
+#endif
+}
